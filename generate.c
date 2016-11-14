b0ffa79f6847dd44d318ed191db82bdc0719bb7c
Tomas Kopta <optagon@users.noreply.github.com>
2016-11-14 22:53:39 +0100

    Create generate.c

---
+++ generate.c
@@ -0,0 +1,41 @@
+#define _XOPEN_SOURCE
+
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// constant
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+   
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: generate n [s]\n");
+        return 1;
+    }
+
+   
+    int n = atoi(argv[1]);
+
+   
+    if (argc == 3)
+    {
+        srand48((long int) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long int) time(NULL));
+    }
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
